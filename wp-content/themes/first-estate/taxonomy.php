336282ff731d5effc5f88d4a8e178c390a509bba
Ilias Johri <ilias.johri@gmail.com>
2012-01-27 21:59:29 +0100

    nieuwe pand

--- wp-content/themes/first-estate/taxonomy.php
+++ wp-content/themes/first-estate/taxonomy.php
@@ -2,42 +2,45 @@
 
 	<section id="content">
 		<div class="container">
-			<div class="grid_3">
+      <section class="cf main_details clear overview">
 				
 				<?php if (have_posts()) : ?>
 
-				<?php while (have_posts()) : the_post(); ?>
-        	
-					<p id="post-<?php the_ID(); ?>">
-						<a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-					</p>
-					
-					<?php echo get_the_post_thumbnail(); ?>
-					
-					<p>
-        		<?php the_field('adres'); ?> <?php the_field('nummer'); ?>, <?php the_field('gemeente'); ?>
-        	</p>
-        	
-        	<p>
-        		<?php the_field('beschrijving'); ?>
-        	</p>
-        	
-        	<p>
-        		<?php the_field('type'); ?><br />
-        		&euro; <?php the_field('prijs'); ?>
-        	</p>
-        	
-        	<hr />
+  				<?php while (have_posts()) : the_post(); ?>
+            
+            <div class="grid_1 pand koop" id="post-<?php the_ID(); ?>">
+              <div class="img_container">
+                <a href="<?php the_permalink() ?>">
+                 
+                  <!-- THUMBNAIL -->
+                  <?php //echo get_the_post_thumbnail(); ?>
+                  <?php 
+                  $thumb = wp_get_attachment_image_src( get_post_thumbnail_id(),'thumbnail' );
+                  //print_r($thumb);
+                  ?> 
+                  <img src="<?php echo $thumb[0]; ?>">
 
-				<?php endwhile; ?>
-			
-				<?php else : ?>
+                  <div class="caption">
+                    <h3><?php the_field('type'); ?></h3>
+                    <p>&euro; <?php the_field('prijs'); ?></p>
+                  </div>
+                </a>
+              </div><!-- .img_container -->
+              <h3><?php the_title(); ?></h3>
+              <h4><?php the_field('adres'); ?> <?php the_field('nummer'); ?>, <?php the_field('gemeente'); ?></h4>
+              <p><?php the_field('beschrijving'); ?></p>
+              <a href="<?php the_permalink() ?>" class="btn">Bekijk dit pand</a>
+            </div><!-- .pand .koop -->
+            
+  				<?php endwhile; ?>
+  			
+  				<?php else : ?>
 
-					<h3>Niets gevonden</h3>
+  					<h3>Niets gevonden</h3>
 
 				<?php endif; ?>
-
-			</div><!-- .grid_3 -->
+      
+      </section>
 		</div><!-- .container -->
 	</section><!-- #content -->
 
