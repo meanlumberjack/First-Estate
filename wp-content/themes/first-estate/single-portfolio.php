6d72e692ab914de9d3e36fad2cc10f373c04e6dd
Tiziano Di Turi <tiziano.dituri@gmail.com>
2012-02-01 20:21:15 +0100

    aanpassing taxonomy & single-portfolio templates

--- wp-content/themes/first-estate/single-portfolio.php
+++ wp-content/themes/first-estate/single-portfolio.php
@@ -21,7 +21,7 @@
       <div class="grid_1 summary">
         <h3><?php the_title(); ?></h3>
         <h4>
-        	<?php echo $adres ?> <?php echo $nummer ?>, <?php echo $gemeente ?>
+        	<?php echo $adres ?> <?php echo $nummer ?> <?php echo $gemeente ?>
         </h4>
         <?php echo $opsomming ?>
         <div class="contact">
@@ -60,21 +60,27 @@
 			
 			<!-- BLOCKS --> 
       <section class="cf blocks">
-        <div class="grid_1 map">
+			<?php if ($map == '') { ?>
+				<div class="grid_2 map">
+          <h3>Details</h3>
+          <?php echo $beschrijving ?>
+        </div>
+        <div class="grid_1">
+          <blockquote>Voor al mijn vragen kan ik steeds terrecht bij First Estate, overdag tijdens kantooruren en zelfs tijdens het weekend. Dit is pas echte service!</blockquote>
+					<cite>Jeanine D'Hoore</cite>
+        </div>
+			<?php } else { ?>
+				<div class="grid_1 map">
           <h3>Details</h3>
           <div class="iframe_container">
-						<?php if ($map == '') { ?>
-							<blockquote>Voor al mijn vragen kan ik steeds terrecht bij First Estate, overdag tijdens kantooruren en zelfs tijdens het weekend. Dit is pas echte service!</blockquote>
-							<cite>Jeanine D'Hoore</cite>
-						<?php } else { ?>
-							<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="<?php echo $map ?>&amp;output=embed&iwloc=near"></iframe>
-						<?php } ?>
+						<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="<?php echo $map ?>&amp;output=embed&iwloc=near"></iframe>
           </div><!-- .iframe_container -->
         </div>
         <div class="grid_2 extra_info">
           <?php echo $beschrijving ?>
         </div>
-      </section>
+			<?php } ?>
+			</section>
 			
 		<?php endwhile; endif; ?>
 		
